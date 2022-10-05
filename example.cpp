91b6c35a02ef991632ab5a70de20b8a87b74c126
Jemtaly <83796250+Jemtaly@users.noreply.github.com>
2022-10-05 10:54:00 +0800

    Add files via upload

--- example.cpp
+++ example.cpp
@@ -42,19 +42,12 @@ void choose(int rec, FILE *ifp, FILE *ofp, uint8_t *iv, uint8_t *key) {
 int main(int argc, char *argv[]) {
 	int rec = 0;
 	FILE *ifp = stdin, *ofp = stdout;
-	uint8_t key[32] = {}, civ[16] = {};
+	uint8_t *key = NULL, *civ = NULL;
 	for (int i = 1; (rec & REC_ERR) == 0 && i < argc; i++) {
 		if (argv[i][0] == '-') {
-			if (argv[i][1] == 'k' && argv[i][2] == '\0') {
-				if ((rec & REC_KEY) == 0 && i + 1 < argc) {
-					strncpy((char *)key, argv[++i], 32);
-					rec |= REC_KEY;
-				} else {
-					rec |= REC_ERR;
-				}
-			} else if (argv[i][1] == 'c' && argv[i][2] == '\0') {
+			if (argv[i][1] == 'c' && argv[i][2] == '\0') {
 				if ((rec & (REC_CTR | REC_DEC | REC_ENC)) == 0 && i + 1 < argc) {
-					strncpy((char *)civ, argv[++i], 16);
+					strncpy((char *)(civ = new uint8_t[16]), argv[++i], 16);
 					rec |= REC_CTR;
 				} else {
 					rec |= REC_ERR;
@@ -72,25 +65,29 @@ int main(int argc, char *argv[]) {
 					rec |= REC_ERR;
 				}
 			} else if (argv[i][1] == 'S' && argv[i][2] == '\0') {
-				if ((rec & (REC_SM4 | REC_128 | REC_192 | REC_256)) == 0) {
+				if ((rec & (REC_SM4 | REC_128 | REC_192 | REC_256)) == 0 && i + 1 < argc) {
+					strncpy((char *)(key = new uint8_t[16]), argv[++i], 16);
 					rec |= REC_SM4;
 				} else {
 					rec |= REC_ERR;
 				}
 			} else if (argv[i][1] == '4' && argv[i][2] == '\0') {
-				if ((rec & (REC_SM4 | REC_128 | REC_192 | REC_256)) == 0) {
+				if ((rec & (REC_SM4 | REC_128 | REC_192 | REC_256)) == 0 && i + 1 < argc) {
+					strncpy((char *)(key = new uint8_t[16]), argv[++i], 16);
 					rec |= REC_128;
 				} else {
 					rec |= REC_ERR;
 				}
 			} else if (argv[i][1] == '6' && argv[i][2] == '\0') {
-				if ((rec & (REC_SM4 | REC_128 | REC_192 | REC_256)) == 0) {
+				if ((rec & (REC_SM4 | REC_128 | REC_192 | REC_256)) == 0 && i + 1 < argc) {
+					strncpy((char *)(key = new uint8_t[24]), argv[++i], 24);
 					rec |= REC_192;
 				} else {
 					rec |= REC_ERR;
 				}
 			} else if (argv[i][1] == '8' && argv[i][2] == '\0') {
-				if ((rec & (REC_SM4 | REC_128 | REC_192 | REC_256)) == 0) {
+				if ((rec & (REC_SM4 | REC_128 | REC_192 | REC_256)) == 0 && i + 1 < argc) {
+					strncpy((char *)(key = new uint8_t[32]), argv[++i], 32);
 					rec |= REC_256;
 				} else {
 					rec |= REC_ERR;
@@ -114,17 +111,12 @@ int main(int argc, char *argv[]) {
 			rec |= REC_ERR;
 		}
 	}
-	if ((rec & (REC_CTR | REC_DEC | REC_ENC)) == 0 || (rec & (REC_SM4 | REC_128 | REC_192 | REC_256)) == 0 || (rec & REC_KEY) == 0 || (rec & REC_ERR) != 0) {
-		fprintf(stderr, "usage: %s [-i INFILE] [-o OUTFILE] (-c IV | -e | -d) (-S | -4 | -6 | -8) -k KEY\n", argv[0]);
-		if ((rec & REC_IFP) != 0) {
-			fclose(ifp);
-		}
-		if ((rec & REC_OFP) != 0) {
-			fclose(ofp);
-		}
-		return 1;
+	if ((rec & (REC_CTR | REC_DEC | REC_ENC)) == 0 || (rec & (REC_SM4 | REC_128 | REC_192 | REC_256)) == 0) {
+		rec |= REC_ERR;
 	}
-	if ((rec & REC_SM4) != 0) {
+	if ((rec & REC_ERR) != 0) {
+		fprintf(stderr, "usage: %s [-i INFILE] [-o OUTFILE] (-c IV | -e | -d) (-S KEY | -4 KEY | -6 KEY | -8 KEY)\n", argv[0]);
+	} else if ((rec & REC_SM4) != 0) {
 		choose<SM4>(rec, ifp, ofp, civ, key);
 	} else if ((rec & REC_128) != 0) {
 		choose<AES128>(rec, ifp, ofp, civ, key);
@@ -133,11 +125,17 @@ int main(int argc, char *argv[]) {
 	} else if ((rec & REC_256) != 0) {
 		choose<AES256>(rec, ifp, ofp, civ, key);
 	}
+	if (key) {
+		delete[] key;
+	}
+	if (civ) {
+		delete[] civ;
+	}
 	if ((rec & REC_IFP) != 0) {
 		fclose(ifp);
 	}
 	if ((rec & REC_OFP) != 0) {
 		fclose(ofp);
 	}
-	return 0;
+	return rec & REC_ERR;
 }
